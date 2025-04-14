086052d0922949f25ba65bdd5c329924acee9af6
Mohammad <ma@bornak.de>
2025-04-14 17:44:17 +0330

    # Added BadgeView with animation also  EnvironmentKey, EnvironmentValues

---
+++ SwiftUIProject/BadgeView.swift
@@ -0,0 +1,18 @@
+//
+//  BadgeView.swift
+//  SwiftUIProject
+//
+//  Created by Mohammad on 4/14/25.
+//
+
+import SwiftUI
+
+struct BadgeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BadgeView()
+}
