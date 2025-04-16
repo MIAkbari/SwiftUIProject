acf4d905abe3b8f0aa361c22321438cd3641ab18
Mohammad <ma@bornak.de>
2025-04-16 21:43:11 +0330

    # Added code animation github

--- SwiftUIProject/GithubView.swift
+++ SwiftUIProject/GithubView.swift
@@ -7,12 +7,63 @@
 
 import SwiftUI
 
-struct GithubView: View {
+struct ContributionsView: View {
+    @State private var isAnimating = false
+    
+    let weeks = 23 // I only used 23 weeks of data because it fits better in the view
+    let daysPerWeek = 7 // 7 days a week
+    var rows: Array<GridItem> { Array(repeating: GridItem(.flexible()), count: daysPerWeek) }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+                Group {
+                    Text("..........")
+                    Image(.mark)
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                }
+                .foregroundStyle(isAnimating ? .indigo : .gray)
+                .font(.largeTitle.weight(.semibold))
+                .animation(.easeInOut(duration: 1.0)
+                        .delay(0.01)
+                        .repeatForever(autoreverses: true), value: isAnimating)
+               .frame(width: isAnimating ? 100 : 0)
+            
+            Spacer().frame(height: 50)
+            
+            HStack {
+                LazyHGrid(rows: rows, spacing: 2) {
+                    ForEach(0..<weeks*daysPerWeek, id: \.self) { item in
+                        RoundedRectangle(cornerRadius: 2)
+                            .fill(isAnimating ? Color("gitColor") : (item % 5 == 0 ? Color("gitColor") : item % 4 == 0 ? .green : .green.opacity(0.4)))
+                            .frame(width: 15, height: 15)
+                            .animation(
+                                .easeInOut(duration: 1.0)
+                                .delay(Double(item) * 0.02)
+                                .repeatForever(autoreverses: true),
+                                value: isAnimating
+                            )
+                    }
+                }
+                .padding(.horizontal, 5)
+                .frame(height: 120)
+               // .drawingGroup() // if use this cant see this animation
+            }
+            
+            Spacer()
+        }
+        .onAppear {
+            Task {
+                await MainActor.run {
+                    withAnimation {
+                        isAnimating = true
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    GithubView()
+    ContributionsView()
 }
