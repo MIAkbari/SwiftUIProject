36ceae80f018df7d49fe1d3454d355ebed6370b1
Mohammad <ma@bornak.de>
2025-04-16 21:42:52 +0330

    ## Added github cons github

---
+++ SwiftUIProject/GithubView.swift
@@ -0,0 +1,18 @@
+//
+//  GithubView.swift
+//  SwiftUIProject
+//
+//  Created by Mohammad on 4/16/25.
+//
+
+import SwiftUI
+
+struct GithubView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GithubView()
+}
